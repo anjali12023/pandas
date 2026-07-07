bd0806781ec2477f50854625cc2f0e73882e0eea
anjali12023 <4njalisharma007@gmail.com>
2026-07-07 12:37:32 +1000

    using series with python dictionaries

--- panda_practise.py
+++ panda_practise.py
@@ -2,10 +2,15 @@ import pandas as pd
 
 #series -- a 1D labelled array 
 
-data = [100, 102, 104, 200, 202]
+# data = [True, 102, 104, 200, 202]
 
-series = pd.Series(data, index=["a", "b", "c", "d", "e"])
+# series = pd.Series(data, index=["a", "b", "c", "d", "e"])
 
 
-print(series[series >= 200])
-#filter by values within the series with condition
+# print(series[series < 200])
+
+#create a python dict to record how many calories eaten a day
+calories = {"Day 1": 1750, "Day 2": 2100, "Day 3": 1700}
+
+series = pd.Series(calories)
+print(series)
