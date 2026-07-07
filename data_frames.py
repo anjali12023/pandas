7d6fab3b5b49406d5ec5e95f959ca87ba8e720f5
anjali12023 <4njalisharma007@gmail.com>
2026-07-07 14:20:52 +1000

    added new row to data frame

--- data_frames.py
+++ data_frames.py
@@ -16,4 +16,12 @@ df = pd.DataFrame(data, index = ["Employee 1", "Employee 2", "Employee 3"])
 # add a new column
 df["Job"] = ["Doctor", "Cook", "N/A"]
 
+# add a new row -- create a new df and then concatenate
+#for each new row, a new dictionary is required
+new_row = pd.DataFrame([{"Name": "Sandy", "Age": 28, "Job": "Scientist"}], 
+                       index = ["Employee 4"])
+
+#concatenate to existing df
+df = pd.concat([df, new_row])
+
 print(df)
