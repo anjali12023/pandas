7541950365d6b1113039f5edeb9060dbad10cee7
anjali12023 <4njalisharma007@gmail.com>
2026-07-07 14:17:37 +1000

    added a new column

--- data_frames.py
+++ data_frames.py
@@ -8,6 +8,12 @@ data = {"Name": ['Anjali', 'Aanchal', 'Navraj'],
 }
 
 #converting dictionary into data frame
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index = ["Employee 1", "Employee 2", "Employee 3"])
+
+# print(df.loc["Employee 1"])
+# print(df.iloc[0])
+
+# add a new column
+df["Job"] = ["Doctor", "Cook", "N/A"]
 
 print(df)
