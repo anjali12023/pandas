f451ffe6843577b11d0f62fa9a0a7bb1dbddfc5c
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 13:23:23 +1000

    aggregate functions for whole data frame

---
+++ aggregation.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+#aggregate functions reduce a set of values into a single summary cvalue 
+# used to summarise and analyse data (often used with groupby() function)
+
+df = pd.read_csv("data.csv")
+
+# WHOLE DATA FRAME 
+#find the mean of any columns that are numeric 
+# print(df.mean(numeric_only =True))
+# print(df.sum(numeric_only =True))
+# print(df.min(numeric_only =True))
+# print(df.max(numeric_only =True))
+print(df.count()) #counting number of values within each column 
+
+#SINGLE DATAFRAME
