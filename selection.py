a947468b48515327816834b746471ce65fe225d7
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 12:48:09 +1000

    selection for multiple columns

--- selection.py
+++ selection.py
@@ -2,4 +2,7 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 
 #selection by column: 
-print(df["Name"])
+# print(df["Name"].to_string())
+
+#selection by columns:
+print(df[["Name", "Height", "Weight"]].to_string())
