0e9b3ac57682ef66fa5c45ff717e28bf440439cc
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 13:03:31 +1000

    selecting row by name

--- selection.py
+++ selection.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-df = pd.read_csv("data.csv")
+# df = pd.read_csv("data.csv")
 
 #selection by column: 
 # print(df["Name"].to_string())
@@ -8,4 +8,9 @@ df = pd.read_csv("data.csv")
 # print(df[["Name", "Height", "Weight"]].to_string())
 
 #Selection by row:
-print(df.loc[0])
+# print(df.loc[0])
+
+#selection by Name
+df = pd.read_csv("data.csv", index_col = "Name")
+
+print(df.loc["Pikachu"])
