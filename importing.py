3474243ec94229c6cae8001364a08111ccaa9e4d
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 12:43:52 +1000

    printing data from csv and json file

--- importing.py
+++ importing.py
@@ -1,2 +1,8 @@
 import pandas as pd
 
+df = pd.read_csv("data.csv")
+df = pd.read_json("data.json") #reading json file 
+
+
+print(df) #truncted version (first and last 5 rows)
+print(df.to_string()) #prints everything
