69677618e7a9907842d350bcfa078c2ba995b1dc
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 13:17:41 +1000

    filtering with two conditions using or operation

--- filtering.py
+++ filtering.py
@@ -3,5 +3,7 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 
 #Filtering = Keeping the rows that match a condition
-tall_pokemon = df[df["Height"] >= 2]
-print(tall_pokemon)
+# tall_pokemon = df[df["Height"] >= 2]
+water_pokemon = df[(df["Type1"] == "Water")|
+                   (df["Type2"] == "Water")]
+print(water_pokemon)
