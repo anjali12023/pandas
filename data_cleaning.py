4794329e16ec8ed51538aba704e939399f0c7eb5
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 17:18:14 +1000

    changing data types + standarising text

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,9 +12,17 @@ df = pd.read_csv("data.csv")
 
 # df = df.fillna({"Type2": "None"})
 
-#3. Fix inconsistent values
+#3. Fix inconsistent valuess
 #replace grass with GRASS in type 1
-df["Type1"] = df["Type1"].replace({"Grass": "GRASS", "Fire": "FIRE",
-                                   "Water": "WATER"})
+# df["Type1"] = df["Type1"].replace({"Grass": "GRASS", "Fire": "FIRE",
+                                #    "Water": "WATER"})
+                            
+#4. Standarise text
+#make all characters lowercase
+df["Name"] = df["Name"].str.lower()
+
+#5. Fix/ change data types
+#Change Legendary to be Booleans 
+df["Legendary"] = df["Legendary"].astype(bool)
 
 print(df.to_string())
