003b8827e6a40a73212d76dd398ef373a117bd66
anjali12023 <4njalisharma007@gmail.com>
2026-07-11 13:18:33 +1000

    filtering two conditions with and operation

--- filtering.py
+++ filtering.py
@@ -6,4 +6,8 @@ df = pd.read_csv("data.csv")
 # tall_pokemon = df[df["Height"] >= 2]
 water_pokemon = df[(df["Type1"] == "Water")|
                    (df["Type2"] == "Water")]
-print(water_pokemon)
+
+
+ff_pokemon = df[(df["Type1"] == "Fire") &
+                   (df["Type2"] == "Flying")]
+print(ff_pokemon)
